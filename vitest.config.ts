dd54d50776ddaf661fc106e70a60c28f645da71e
Erik Müller <emueller@newstore.com>
2022-12-01 09:43:33 +0100

    Coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,10 @@
-import { configDefaults, defineConfig } from 'vitest/config'
+import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {
-    exclude: [...configDefaults.exclude, 'build'],
-  },
-})
+	test: {
+		exclude: [...configDefaults.exclude, "build"],
+		coverage: {
+			reporter: ["text", "html", "cobertura"],
+		},
+	},
+});
